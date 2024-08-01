1d9a4d3a4097ce6e49d5daa90f3d9dc59247810e
南原　昭太 <shota@nanbarashoutanoMacBook-Air.local>
2024-08-01 21:44:00 +0900

    Update some files

--- app/Http/Controllers/API/SalesController.php
+++ app/Http/Controllers/API/SalesController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Models\Product;
 use App\Models\Sale;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 
 class SalesController extends Controller
